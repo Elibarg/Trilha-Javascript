f6797dacba0c7ba4f932817f09bb1a6344bb5102
Elibarg <calagabriel31@gmail.com>
2025-04-24 19:45:09 -0300

    first commit

---
+++ OT 01 - JAVASCRIPT/index.js
@@ -0,0 +1,8 @@
+var texto1 ="Meu nome";
+var nome ="Gabriel";
+var texto2 =" ,eu tenho";
+var idade = 19;
+var texto3 ="anos, e minha comida favorita é";
+var comida = "sushi"
+
+console.log(texto1+" "+nome+" "+texto2+" "+idade+" "+texto3+" "+comida);
