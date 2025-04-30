72526cad3402563f7103f21d768b8f667045f360
Elibarg <calagabriel31@gmail.com>
2025-04-29 21:48:02 -0300

    ear

--- OT 04 - JAVASCRIPT/index.js
+++ OT 04 - JAVASCRIPT/index.js
@@ -1,6 +1,54 @@
-if (idade < 13) {
-    categoria.unshift(criança)
-}else if (idade => 13 )
-{
+let idade = 20; 
+let categoria;
 
+if (idade < 13) {
+    categoria = 'criança';
+} else if (idade >= 13 && idade <= 17) {
+    categoria = 'adolescente';
+} else if (idade >= 18 && idade <= 64) {
+    categoria = 'adulto';
+} else {
+    categoria = 'idoso';
+}
+
+console.log(categoria);
+
+
+let adiv = 3; // Suponha que o usuário escolheu o número 3
+let numeroCorreto = 3;
+
+switch (adiv) {
+    case 1:
+    case 2:
+    case 4:
+    case 5:
+        console.log("Não acertou");
+        break;
+    case 3:
+        console.log("Acertou!");
+        break;
+    default:
+        console.log("Número inválido");
+        break;
+}
+
+switch (adiv) 
+{
+    case 1:
+    case 2:
+    case 4:
+    case 5:
+        console.log("Reprovado");
+    break;
+
+    case 6:
+    case 7:
+        console.log("Recuperação");
+    break;
+
+    case 8:
+        console.log("Aprovado");
+    break;
+    default:
+    break;
 }
