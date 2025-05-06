1ec5335ec1c189d1b7540cbc9cbba666b0f954df
Elibarg <calagabriel31@gmail.com>
2025-05-05 21:42:34 -0300

    er

---
+++ OT 06 - JAVASCRIPT/index.js
@@ -0,0 +1,10 @@
+let carro ={
+   marca: 'chevrolet', 
+   modelo: 'Equinox Turbo',
+   ano: 2025
+}
+   console.log(carro.modelo);
+
+   carro.ano = 2026;
+   carro.cor = 'verde';
+   console.log(carro.ano,carro.cor);
