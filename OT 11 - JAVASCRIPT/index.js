9ff94aefd7e61621d0b68ef45a59e4f1781ec4e7
Elibarg <calagabriel31@gmail.com>
2025-05-19 21:37:45 -0300

    terminar10e11

---
+++ OT 11 - JAVASCRIPT/index.js
@@ -0,0 +1,5 @@
+let nome = "Jó";
+let dia = "22/03";
+let evento = "aniversário";
+let mensagem = "Sua presença é o maior presente que posso receber no meu "+evento+" Conto com você! "+nome+" no dia:"+dia;
+console.log(mensagem);
