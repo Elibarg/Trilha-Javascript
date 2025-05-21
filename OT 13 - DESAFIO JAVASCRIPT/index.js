0cceab8044f1d8b7039e8ebf9063e517c2fba71a
Elibarg <calagabriel31@gmail.com>
2025-05-20 21:38:27 -0300

    ot12

---
+++ OT 13 - DESAFIO JAVASCRIPT/index.js
@@ -0,0 +1,14 @@
+function soma(a,b)
+{
+    return a + b;
+}
+let resultado = soma(10,2);
+console.log(resultado)
+
+let soma2 = resultado+5;
+console.log(soma2)//17
+
+function soma3(a,b)
+{
+    return a + b;
+}
